34bfea65ad52cae204cc7ca6ee14b8a4171224ab
Adam Gahverdiev <adam_gahverdiev@mail.ru>
2024-01-15 23:58:46 +0300

    wip: added test for task 1

--- develop/dev01/task_test.go
+++ develop/dev01/task_test.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestGetTime(t *testing.T) {
+	time := ntpTime{}
+	got, err := time.getTime()
+	if err != nil {
+		t.Errorf("getTime() error = %v", err)
+	}
+	if got.IsZero() {
+		t.Errorf("getTime() = %v, want non-zero time", got)
+	}
+}
