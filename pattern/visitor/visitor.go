6429c43219678c802448de185a9dd8b0e62bb6b1
Adam <adam_gahverdiev@mail.ru>
2024-01-24 03:59:35 +0300

    wip: finished the implementation of the visitor pattern

--- pattern/visitor/visitor.go
+++ pattern/visitor/visitor.go
@@ -5,3 +5,31 @@ package pattern
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Visitor_pattern
 */
+
+type Element struct {
+	Accept (Checker)
+}
+
+type Checker interface {
+	CheckCPU(*CPU)
+	CheckHardDrive(*HardDrive)
+	CheckMemory(*Memory)
+}
+
+type CPU struct{}
+
+func (c *CPU) Accept(c Checker) {
+	c.CheckCPU(c)
+}
+
+type HardDrive struct{}
+
+func (h *HardDrive) Accept(c Checker) {
+	c.CheckHardDrive(h)
+}
+
+type Memory struct{}
+
+func (m *Memory) Accept(c Checker) {
+	c.CheckMemory(m)
+}
