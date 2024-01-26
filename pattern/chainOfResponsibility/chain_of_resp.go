10d67eb8a37a46cb62b0e8b42f6b5df07efed084
Adam <adam_gahverdiev@mail.ru>
2024-01-26 04:40:19 +0300

    wip: finished the implementation of the chain of responsibility pattern

--- pattern/chainOfResponsibility/chain_of_resp.go
+++ pattern/chainOfResponsibility/chain_of_resp.go
@@ -1,7 +1,48 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «цепочка вызовов».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Chain-of-responsibility_pattern
 */
+
+type Handler interface {
+	SetNext(Handler)
+	Handle(string)
+}
+
+type ConcretedHandler1 struct {
+	next Handler
+}
+
+func (h *ConcretedHandler1) SetNext(next Handler) {
+	h.next = next
+}
+
+func (h *ConcretedHandler1) Handle(request string) {
+	if request == "run" {
+		fmt.Println("ConcretedHandler1: Running")
+
+	} else if h.next != nil {
+		h.next.Handle(request)
+	}
+}
+
+type ConcretedHandler2 struct {
+	next Handler
+}
+
+func (h *ConcretedHandler2) SetNext(next Handler) {
+	h.next = next
+}
+
+func (h *ConcretedHandler2) Handle(request string) {
+	if request == "stop" {
+		fmt.Println("ConcretedHandler1: Stopping")
+
+	} else if h.next != nil {
+		h.next.Handle(request)
+	}
+}
