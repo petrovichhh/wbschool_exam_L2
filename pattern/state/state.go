78372f3756db589551b55bf08af8c404f898e55c
Adam <adam_gahverdiev@mail.ru>
2024-01-28 21:54:56 +0300

    wip: finished the implementation of the state pattern

--- pattern/state/state.go
+++ pattern/state/state.go
@@ -1,7 +1,39 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «состояние».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/State_pattern
 */
+
+type State interface {
+	Handle() error
+}
+
+type WaitingState struct{}
+
+func (s *WaitingState) Handle() error {
+	fmt.Println("Waiting for input")
+	return nil
+}
+
+type ServingState struct{}
+
+func (d *ServingState) Handle() error {
+	fmt.Println("Serving the drink")
+	return nil
+}
+
+type VendingMachine struct {
+	state State
+}
+
+func (v *VendingMachine) SetState(state State) {
+	v.state = state
+}
+
+func (v *VendingMachine) Handle() error {
+	return v.state.Handle()
+}
